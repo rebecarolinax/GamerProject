c7af912f2db799855bac52b6ec5843d45132637e
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-10-31 14:37:15 -0300

    :tada: Projeto Gamer

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace projeto_gamer_mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
